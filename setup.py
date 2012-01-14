1108ada99d653710429c2243e42852f5b5469dd6
Fumiaki Kinoshita <fumiexcel@gmail.com>
2012-01-14 15:02:45 +0900

    changed package information

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="Curtana",
-    version="0.1.0",
+    version="0.2.1",
     description="Twitter Client for Chosen One",
     license="BSD",
     keywords="Twitter",
     url="http://botis.org/wiki/Curtana",
     packages=find_packages(),
-    install_requires = ["oauth2>=1.5", "tweepy>=1.7"],
+    install_requires = ["oauth2>=1.5", "twython>=1.4"],
     include_package_data=True,
 )
