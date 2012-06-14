0469a350f27cf8ada00d41ead3fad49d324f5fbc
fumieval <fumiexcel@gmail.com>
2012-06-14 11:07:55 +0900

    Update master

--- curtana/lib/mixin.py
+++ curtana/lib/mixin.py
@@ -31,3 +31,4 @@ def InfixMix(left="left", right="right"):
         self.__dict__[right] = right
     def __repr__(self):
         return "(%r %s %r)" % (self.__dict__[left], self.__class__.op, self.__dict__[right])
+    return type("InfixMix({!r}, {!r})".format(left, right), (object,), {"__init__": __init__, "__repr__": __repr__})
